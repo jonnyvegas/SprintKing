b37f79f9a9bfcda1c71f900072a935cd14718f40
THE-BEAST\jonat <jonathan.e.villegas@gmail.com>
2026-04-09 16:48:18 -0700

    Added Run animation. Added to state machine animation. Added transition via Hit. Added transition when hit to play hit animation. Added cooldown check to make sure that we can stumble. Made default -1.5 so that we stumble right out of the gate, if needed.

--- Sprint King/Assets/Scripts/Collision/PlayerCollisionHandler.cs
+++ Sprint King/Assets/Scripts/Collision/PlayerCollisionHandler.cs
@@ -2,10 +2,21 @@ using UnityEngine;
 
 public class PlayerCollisionHandler : CollisionHandler
 {
+    [SerializeField] Animator animator;
+    const string hitString = "Hit";
+    [SerializeField] float hitCooldown = .5f;
+    float currentTime = -1.5f;
     public override void OnCollisionEnter(Collision collision)
     {
         base.OnCollisionEnter(collision);
-        Debug.Log("Player collision handler. collided with " + collision.gameObject.name);
+
+        
+        if (Time.time - currentTime > hitCooldown)
+        {
+            currentTime = Time.time;
+            animator.SetTrigger(hitString);
+        }
+
     }
     public override void OnTriggerEnter(Collider other)
     {
