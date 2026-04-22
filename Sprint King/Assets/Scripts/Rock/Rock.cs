1295aaf388f9c2898fdfed575b36b41f1800d646
THE-BEAST\jonat <jonathan.e.villegas@gmail.com>
2026-04-22 13:17:15 -0700

    Added code to spawn rock particle effects (mesh particle) and added audio source to the rock to play the sound when it hits the ground.

--- Sprint King/Assets/Scripts/Rock/Rock.cs
+++ Sprint King/Assets/Scripts/Rock/Rock.cs
@@ -5,16 +5,40 @@ public class Rock : MonoBehaviour
 {
     CinemachineImpulseSource cinemachineImpulseSource;
     [SerializeField] float shakeModifier = 10f;
+    [SerializeField] ParticleSystem rockParticles;
+    [SerializeField] AudioClip rockClip;
+    AudioSource rockSource;
     private void Awake()
     {
         cinemachineImpulseSource = GetComponent<CinemachineImpulseSource>();
+        rockSource = GetComponent<AudioSource>();
+        rockSource.clip = rockClip;
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void FireImpulse()
     {
         float distance = Vector3.Distance(transform.position, Camera.main.transform.position);
         float shakeIntensity = (1f / distance) * shakeModifier;
         shakeIntensity = Mathf.Min(shakeIntensity, 1);
         cinemachineImpulseSource.GenerateImpulse(shakeIntensity);
+    }
+
+    private void CollisionFX(Collision collision)
+    {
+        if(rockParticles)
+        {
+            rockParticles.transform.position = collision.GetContact(0).point;
+            rockParticles.Play();
+        }
+        if(rockSource && rockClip)
+        {
+            rockSource.Play();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        FireImpulse();
+        CollisionFX(collision);
     }
 }
